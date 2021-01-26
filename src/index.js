1eef3534c380ae503387cabd0ea907709ad8b487
ZeeshanAli309 <78010556+ZeeshanAli309@users.noreply.github.com>
2021-01-26 19:04:48 +0500

    yes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>hyee </h1>,document.getElementById("root"))
+ReactDOM.render(<h1>hyee i am soooo happyy </h1>,document.getElementById("root"));
