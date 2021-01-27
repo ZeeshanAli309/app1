81cd5f6521c65860c4d3ae5b8f37bcefcc223c97
ZeeshanAli309 <78010556+ZeeshanAli309@users.noreply.github.com>
2021-01-27 14:29:41 +0500

    deploying with App

---
+++ src/greeting.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+function Greeting(){
+    return(<h1>hello guys </h1>);
+        
+    
+}
+export default Greeting ;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Greeting from "./greeting"
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<h1>hyee i am soooo happyy </h1>,document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Greeting/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
